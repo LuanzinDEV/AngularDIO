0a901810759adb8524a7cd9f13b0f24a430c606c
Luan Tobias <luantobiasdasilveiracamposdev@gmail.com>
2025-04-20 17:29:25 -0300

    leran how to apply style and append child in the components

---
+++ SHADOW DOM/src/Componets/TextCustom.js
@@ -0,0 +1,30 @@
+class TextCustom extends HTMLElement {
+    constructor(){
+        super();
+
+        const shadow = this.attachShadow({mode: "open"})
+
+        // text 
+
+        const componentRoot = document.createElement("h1")
+        componentRoot.textContent = "Luan"
+
+        // style
+
+        const style = document.createElement("style")
+
+        style.textContent = `
+            h1{
+                color: red;
+            }
+        `
+
+        // apply the ELEMENT to the component 
+
+        shadow.appendChild(componentRoot)
+        shadow.appendChild(style)
+
+    }
+}
+
+customElements.define('text-custom', TextCustom)
